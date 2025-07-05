b7d3979b978e0016056fba8cb3717fe4e6f38cc4
Vandana-star-png <vandanacreddy6543@gmail.com>
2025-07-05 14:41:19 +0530

    Patient and Patient Request model is added with validations and EF data annotation attributes

---
+++ PatientManagement/PatientManagement/Data/Patient.cs
@@ -0,0 +1,20 @@
+﻿namespace PatientManagement.Data
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string ContactNumber { get; set; }
+        public float Weight { get; set; }
+        public float Height { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string MedicalComments { get; set; }
+        public bool AnyMedicationsTaking { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
---
+++ PatientManagement/PatientManagement/Models/PatientRequest.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PatientManagement.Models
+{
+    public class PatientRequest
+    {
+        [Required(ErrorMessage = "First name is required")]
+        [MaxLength(50), MinLength(2)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        [MaxLength(50), MinLength(2)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Date of Birth is required")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Gender is required")]
+        [MaxLength(10)]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Contact number is required")]
+        [Phone]
+        [RegularExpression(@"^\+?[0-9]{10, 15}$", ErrorMessage = "Enter a valid contact number")]
+        public string ContactNumber { get; set; }
+
+        [Required(ErrorMessage = "Weight is required")]
+        [Range(1, 200, ErrorMessage = "Weight must be between 1 and 200kg")]
+        public float Weight { get; set; }
+
+        [Required(ErrorMessage = "Height is required")]
+        [Range(1, 250, ErrorMessage = "Height must be between 1 and 250cm")]
+        public float Height { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [MaxLength(20)]
+        public string Address { get; set; }
+
+        [MaxLength(50)]
+        public string MedicalComments { get; set; }
+
+        [Display(Name = "Any Medications Taking")]
+        public bool AnyMedicationsTaking { get; set; }
+    }
+}
